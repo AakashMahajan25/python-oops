09e99da17b1eb1d444fadc3ec5d3bd50b8aa8a00
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-05-03 16:37:45 +0530

    feat: done with polymorphism

--- main.py
+++ main.py
@@ -8,22 +8,25 @@ class Car:
     
     def full_name(self):
         return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
 
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
+    
+    def fuel_type(self):
+        return "Electric Charge"
 
 
 my_tesla = ElectricCar("Tesla", "Model S", "85kWH")
-print(my_tesla.full_name())
-print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
 
 
-# my_car = Car("Toyota", "Corolla")
-# print(my_car.brand)
-# print(my_car.model)
-# print(my_car.full_name())
+my_car = Car("Toyota", "Corolla")
+print(my_car.fuel_type())
 
 # my_new_car = Car("Tata", "Harrier")
 # print(my_new_car.model)
